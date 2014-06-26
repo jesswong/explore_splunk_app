c91efc25c931b1a463fdeb35e496c0c2afd99bdf
Wong, Jessica <jessica.wong@exchange.nordstrom.com>
2014-06-26 10:39:00 -0700

    Added search/graph view of splunk

--- django/explore_splunk_app/urls.py
+++ django/explore_splunk_app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from splunkdj.utility.views import render_template as render
 
 urlpatterns = patterns('',
-    url(r'^home/$', 'explore_splunk_app.views.home', name='home'), 
+    url(r'^home/$', 'explore_splunk_app.views.home', name='home'),
 )
